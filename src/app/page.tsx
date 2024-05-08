cd44343668b8530a977be66773f7e9f14f730295
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-05-08 21:45:53 +0500

    baisc structure (table.tsx)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Navbar from "@/components/Navbar/Navbar"
+import Table from "@/components/Table/Table"
 
 const page = () => {
   return (
-    <div>page</div>
+  <div>
+    <Navbar/>
+    <div className="mt-4 mx-auto max-w-[80%]">
+      <h2 className="text-xl">
+      1302 Pokemons In Total
+    </h2>
+    <Table/>
+    </div>
+  </div>
+
   )
 }
 
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 border-solid border-b-[1px] border-stone-600">
+      <div className="h-[75px] flex justify-between item-center mx-auto max-w-[80%]">
+        <div className="flex item-center">
+          <Image src="logo.svg" width={100} height={100} alt="logo" />
+        </div>
+        <div className="flex item-center">
+          <button>O</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
