41f36c2659eec3fcb1573824c151a97cf622dd51
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-05-09 00:25:31 +0500

    commit

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,73 +1,57 @@
 import Image from "next/image";
-
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+ 
 const Table = () => {
   return (
     <div className="relative my-4 overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-100">
-
-        <thead className="text-xs text-gray-700 uppercase bg-transparent border dark:border-gray-700 dark:text-gray-100">
+        <thead className="text-xs text-gray-100 bg-transparent border">
           <tr>
             <th scope="col" className="px-6 py-3">
               ID
             </th>
             <th scope="col" className="px-6 py-3">
-              <div className="flex items-center">
-                Image
-                <a href="#">
-                  {/* SVG icon */}
-                </a>
-              </div>
+              Image
             </th>
             <th scope="col" className="px-6 py-3">
-              <div className="flex items-center">
-                Name
-                <a href="#">
-                  {/* SVG icon */}
-                </a>
-              </div>
+              Name
             </th>
-            <th scope="col" className="px-6 py-3">
-              <div className="flex items-center justify-center">
-                Action
-                <a href="#">
-                  {/* SVG icon */}
-                </a>
-              </div>
+            <th scope="col" className="flex justify-center px-6 py-3">
+              Action
             </th>
           </tr>
         </thead>
-        <tbody>
-          <tr className="bg-transparent border dark:border-gray-700">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-100 whitespace-nowrap dark:text-white"
-            >
-              1
-            </th>
+        <tbody className="text-gray-100">
+          <tr className="bg-transparent border cursor-pointer hover:bg-sky-700 active:bg-gray-700">
+            <td className="px-6 py-4 font-medium whitespace-nowrap">1</td>
             <td className="px-6 py-4">
-              <Image src="logo.svg" width={50} height={50} alt="logo" />
+              <Image src="/logo.svg" width={50} height={50} alt="logo" />
             </td>
             <td className="px-6 py-4">Laptop</td>
             <td className="px-6 py-4 flex items-center justify-center">
-              {/* Apply bg-transparent to the button's parent element */}
               <button className="bg-transparent">O</button>
             </td>
           </tr>
-          <tr className="bg-transparent border dark:border-gray-700">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-100 whitespace-nowrap dark:text-white"
-            >
-              2
-            </th>
+          <tr className="bg-transparent border cursor-pointer hover:bg-sky-700 active:bg-gray-700">
+            <td className="px-6 py-4 font-medium whitespace-nowrap">2</td>
             <td className="px-6 py-4">
-              <Image src="logo.svg" width={50} height={50} alt="logo" />
+              <Image src="/logo.svg" width={50} height={50} alt="logo" />
             </td>
             <td className="px-6 py-4">Laptop PC</td>
             <td className="px-6 py-4 flex items-center justify-center">
-              {/* Apply bg-transparent to the button's parent element */}
               <button className="bg-transparent">O</button>
             </td>
+          </tr>
+          <tr className="bg-transparent border">
+            <td className="px-6 py-4 font-medium whitespace-nowrap"></td>
           </tr>
         </tbody>
       </table>
