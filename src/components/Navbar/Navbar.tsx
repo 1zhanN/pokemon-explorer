5d7744cf3d92f0cb991b6b597ecd4b80a1ea9469
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-05-13 22:51:52 +0500

    fixed bugs 2.0

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,12 @@
-import { useState } from "react";
+
 import Image from "next/image";
 
 const Navbar = () => {
-  // State variable to track light mode
-  const [isLightMode, setIsLightMode] = useState(false);
-
-  // Function to toggle light mode
-  const toggleLightMode = () => {
-    setIsLightMode(!isLightMode);
-    // Toggle class on body element
-    document.body.classList.toggle("light-mode");
-  };
-
   return (
     <nav className="bg-gray-800 border-solid border-b-[1px] border-stone-600">
       <div className="h-[75px] flex justify-between item-center mx-auto max-w-[80%]">
         <div className="flex item-center">
           <Image src="/logo.svg" width={100} height={100} alt="logo" />
-        </div>
-        <div className="flex item-center">
-          <button onClick={toggleLightMode}>
-            <img src="/nightMode.png" width={20} height={20} alt="darkmode" />
-          </button>
         </div>
       </div>
     </nav>
